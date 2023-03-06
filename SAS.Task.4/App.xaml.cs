d3c1e6f7ec53f0f653d1abd1304b8a673f1471c2
artem <artem@DESKTOP-7S1DBR4>
2023-03-06 20:21:53 +0300

    Добавьте файлы проекта.

---
+++ SAS.Task.4/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SAS.Task._4;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SAS.Task._4
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+    }
+}
---
+++ SAS.Task.4/View/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SAS.Task._4.Core;
+using SAS.Task._4.View.Pages.TaskPages;
+using System.Windows;
+using System.Windows.Controls;
+namespace SAS.Task._4.View.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
